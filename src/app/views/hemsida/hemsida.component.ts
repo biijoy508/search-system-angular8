97fdf382b55a40d0319e59bd77d677008eff2dec
Monir Khan <monir.khan@jordbruksverket.se>
2019-10-23 11:47:25 +0200

    features/ hemsida
    --   ändrat styleguide css anrop, favicon och logo position.
    --   lägget lite 3 parameter i sök komponent

--- src/app/views/hemsida/hemsida.component.ts
+++ src/app/views/hemsida/hemsida.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hemsida.component.scss']
 })
 export class HemsidaComponent implements OnInit {
+  arendetyp = '';
+  stodar = '';
+  anstyp = '';
+  arendetyp_values = ['FARERS', 'SUGGERS', 'KLOVERS'];
+  stodar_values = ['2019', '2018', '2017', '2016'];
+  anstyp_values = ['ÅTAG', 'UTBET'];
 
   constructor() { }
 
