5cae1c1276e78eb6689799d94418cdba6ddd7d8d
Monir Khan <monir.khan@jordbruksverket.se>
2019-10-25 15:04:30 +0200

    Lägger till tabell parametrar som interface i hemsida component.

--- src/app/views/hemsida/hemsida.component.ts
+++ src/app/views/hemsida/hemsida.component.ts
@@ -1,22 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
+interface Arende {
+  kundnr: String;
+  namn: String;
+  stodar: String;
+  arendeTyp: String;
+  ansokansTyp: String;
+  status: String;
+  arendenr: String;
+}
 @Component({
   selector: 'app-hemsida',
   templateUrl: './hemsida.component.html',
   styleUrls: ['./hemsida.component.scss']
 })
+
 export class HemsidaComponent implements OnInit {
   arendetyp = '';
   stodar = '';
   anstyp = '';
   noresult = false;
-  arendetyp_values = ['FARERS', 'SUGGERS', 'KLOVERS'];
-  stodar_values = ['2019', '2018', '2017', '2016'];
-  anstyp_values = ['ÅTAG', 'UTBET'];
+  arendetypValues = ['FARERS', 'SUGGERS', 'KLOVERS'];
+  stodarValues = ['2019', '2018', '2017', '2016'];
+  anstypValues = ['ÅTAG', 'UTBET'];
 
-  constructor() { }
+  arendeLista: Arende[] = [{
+    kundnr: '1',
+    namn: 'Str2ing',
+    stodar: 'Str3ing',
+    arendeTyp: 'Str4ing',
+    ansokansTyp: 'St5ring',
+    status: 'Str6ing',
+    arendenr: 'St7ring'
+  }, {
+      kundnr: '2',
+      namn: 'Str2ing',
+      stodar: 'Str3ing',
+      arendeTyp: 'Str4ing',
+      ansokansTyp: 'St5ring',
+      status: 'Str6ing',
+      arendenr: 'St7ring'
+    }];
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+
   }
   confirmbtnClick() {
     this.noresult = !this.noresult;
