9f8eff7a072dc83fb220c388724341fdad2d75b8
Monir Khan <monir.khan@jordbruksverket.se>
2019-11-11 15:36:22 +0100

    Spinner initialladdning

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,13 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  
   constructor(private httpClient: HttpClient) {
+
   }
+
+  public getChainedData(): Observable<any> {
+    const response1 = this.httpClient.get(environment.stodArUrl);
+    const response2 = this.httpClient.get(environment.arendeTyperUrl);
+    const response3 = this.httpClient.get(environment.ansokansTyperUrl);
+    return forkJoin([response1, response2, response3]);
+  }
+
   public getData(url) {
     return this.httpClient.get(url);
   }
@@ -15,7 +27,9 @@ export class ApiService {
   public getDataMedParametrar(url, parameters) {
     return this.httpClient.get(url, { params: parameters });
   }
+
   public postData(url, data) {
     return this.httpClient.post<any>(url, data);
   }
+
 }
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HemsidaComponent } from './hemsida/hemsida.component';
 import { KundsidaComponent } from './kundsida/kundsida.component';
 import { MassHanteringComponent } from './mass-hantering/mass-hantering.component';
-import { ExempelviewComponent } from './exempelview/exempelview.component';
 import { ViewComponentsModule } from '../view-components/view-components.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -11,11 +10,11 @@ import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [HemsidaComponent, KundsidaComponent, MassHanteringComponent, ExempelviewComponent],
+  declarations: [HemsidaComponent, KundsidaComponent, MassHanteringComponent],
   imports: [
     CommonModule, ViewComponentsModule, FormsModule, ReactiveFormsModule, RouterModule
   ],
-  exports: [HemsidaComponent, KundsidaComponent, MassHanteringComponent, ExempelviewComponent],
+  exports: [HemsidaComponent, KundsidaComponent, MassHanteringComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class ViewsModule { }
