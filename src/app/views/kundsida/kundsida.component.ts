50f95e6814bf2eed0f7859b8a2b71f19503eecc2
Monir Khan <monir.khan@jordbruksverket.se>
2019-11-21 10:32:12 +0100

    demo ansokan djurvalfard'

--- src/app/views/kundsida/kundsida.component.ts
+++ src/app/views/kundsida/kundsida.component.ts
@@ -19,6 +19,12 @@ export class KundsidaComponent implements AfterViewInit {
   kundNummerAlfaNumerisk: any;
   adress: string;
 
+  PPNnummer: string = '43,42,41';
+  antalDjur: string = '321';
+  antalDjurenheter: string = '220';
+
+
+
   constructor(private apiService: ApiService, private route: ActivatedRoute) {
     this.windowRef = window;
     this.arende = new Arende('', '',  '', '', '', '', '', '', '', '');
@@ -30,7 +36,7 @@ export class KundsidaComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.arendeNummer = this.route.snapshot.paramMap.get('arendeNummer')
+    this.arendeNummer = this.route.snapshot.paramMap.get('arendeNummer');
 
     this.kundNummerAlfaNumerisk = this.route.snapshot.paramMap.get('kundNummerAlfaNumerisk');
 
@@ -44,5 +50,30 @@ export class KundsidaComponent implements AfterViewInit {
         this.arende = data[0];
         this.adress = 'Volymgatan 12, 555 55 Volymstad';
       });
+  }
+
+  redigeraView(button: HTMLButtonElement) {
+    if (button.innerText === 'Redigera') {
+      button.innerText = 'Spara';
+      const redigera = document.querySelectorAll('.redigeraView');
+      const vanligt = document.querySelectorAll('.vanligt');
+      for (let i = 0; i < redigera.length; i++) {
+        (redigera[i] as HTMLDivElement).style.display = "block";
+      }
+      for (let j = 0; j < vanligt.length; j++) {
+        (vanligt[j] as HTMLDivElement).style.display = "none";
+      }
+    } else if (button.innerText === 'Spara') {
+      button.innerText = 'Redigera';
+      const redigera = document.querySelectorAll('.redigeraView');
+      const vanligt = document.querySelectorAll('.vanligt');
+      for (let i = 0; i < redigera.length; i++) {
+        (redigera[i] as HTMLDivElement).style.display = "none";
+      }
+      for (let j = 0; j < vanligt.length; j++) {
+        (vanligt[j] as HTMLDivElement).style.display = "block";
+      }
+    }
+
   }
 }
